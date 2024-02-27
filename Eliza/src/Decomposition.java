b5cde7c3d9fa28f465f2e1685217dae899b30a5f
kuroneko-A <152032398+kuroneko-A@users.noreply.github.com>
2024-02-27 15:53:12 +0000

    Add files via upload

---
+++ Eliza/src/Decomposition.java
@@ -0,0 +1,40 @@
+package eliza.wrapper;
+
+import java.util.ArrayList;
+
+/**
+ * Wrapper for the decomposition rules and associated reassembly rules.
+ * 
+ * @author Ariel
+ */
+public class Decomposition {
+	private String rule;
+	private StringArrayRandomizer reassembly;
+
+	/**
+	 * Creates a new Decomposition object.
+	 * 
+	 * @param rule
+	 *            The decomposition rule.
+	 * @param reassembly
+	 *            An ArrayList of reassembly rules.
+	 */
+	public Decomposition(String rule, ArrayList<String> reassembly) {
+		this.rule = rule;
+		this.reassembly = new StringArrayRandomizer(reassembly);
+	}
+
+	public String getRule() {
+		return rule;
+	}
+
+	/**
+	 * Get a random reassembly rule.
+	 * 
+	 * @return A reassembly rule.
+	 */
+	public String getReassembly() {
+		return reassembly.next();
+	}
+
+}
---
+++ Eliza/src/README.md
@@ -0,0 +1,7 @@
+--Running the Eliza Program--
+Make sure the .jar-file is correctly named "eliza.jar", that the script file is in the same folder and is called "script.txt".
+
+java -jar eliza.jar
+
+Requirements:
+Java Runtime Environment needs to be installed.
